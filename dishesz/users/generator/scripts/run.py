6fb4f338ffc84b121c9837e44615c2ad8fb268f2
zeusgod <Patrickattabaah16@gmail.com>
2022-12-15 09:56:15 -0500

    updating run.py in generator

--- dishesz/users/generator/scripts/run.py
+++ dishesz/users/generator/scripts/run.py
@@ -5,17 +5,17 @@ from users.generator.scripts.generate_data import GenerateData
 
 def run(): 
 
-    data = GenerateData(clean_database=False)
-    #data.generate_fake_users(file_directory='generator/json/users.json') 
-    #data.generate_recipes(file_directory='generator/json/recipes.json')
-    #data.generate_ingredients(file_directory='generator/json/ingredients.json')
-    #data.generate_stores(file_directory='generator/json/stores.json')
+    data = GenerateData(clean_database=True)
+    data.generate_fake_users(file_directory='generator/json/users.json') 
+    data.generate_recipes(file_directory='generator/json/recipes.json')
+    data.generate_ingredients(file_directory='generator/json/ingredients.json')
+    data.generate_stores(file_directory='generator/json/stores.json')
     data.generate_reviews(file_directory='generator/json/reviews.json')
-    #data.randomize_follow()
+    data.randomize_follow()
   
     #data.generate_photos()
 
-    #data.add_user_interests() 
+    data.add_user_interests() 
     data.generate_saved_recipes() 
 
 
